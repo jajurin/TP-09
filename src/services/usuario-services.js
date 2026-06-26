805d3cbbee861865b6ea71862b5db24c35b9add4
Android Studio <devandroid@ort>
2026-06-26 08:57:00 -0300

    cambiando nombres

--- src/services/usuario-services.js
+++ src/services/usuario-services.js
@@ -1,23 +1,23 @@
-import usuarioRepository  from '../repositories/repository-province.js';
+import usuarioRepository from '../repositories/usuario-repository.js';
 import { validarProvincia } from '../helpers/validaciones-helper.js';
 
 
-export default class ProvinceService {
+export default class usuarioService {
 
     getAllAsync = async () => {
-        const repo = new ProvinceRepository()   
+        const repo = new usuarioRepository()   
         const returnArray = await repo.getAllAsync()
         return returnArray
     }
 
   getByIdAsync = async (id) => {
 
-    const repo = new ProvinceRepository();
-    const province = await repo.getByIdAsync(id)
-    return province;
+    const repo = new usuarioRepository();
+    const usuario = await repo.getByIdAsync(id)
+    return usuario;
 }
    createAsync = async (body) => {
     validarProvincia(body)
-    const repo = new ProvinceRepository()
+    const repo = new usuarioRepository()
     return await repo.createAsync(body)
 }}
